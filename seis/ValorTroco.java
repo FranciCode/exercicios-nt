978acdbcc4bb516b37e81f6cd7270b0a2b77aa34
Franciele Santos <francielesantosadm@gmail.com>
2023-07-04 18:12:14 -0300

    Ajuste no exercício seis.

--- seis/ValorTroco.java
+++ seis/ValorTroco.java
@@ -1,19 +1,22 @@
 package exerciciosnt.seis;
+import java.math.BigDecimal;
 
 public class ValorTroco {
     public static void main(String[] args) {
-        float custo = 17.35f;
-        float pagamento = 20.0f;
+        double custo = 17.35;
+        double pagamento = 20.0;
         calcularTroco(custo, pagamento);
     }
-    public static void calcularTroco(float custo, float pagamento) {
-        float troco = pagamento - custo;
-        System.out.println("Custo: "+custo);
-        System.out.println("Pagamento: "+pagamento);
-        System.out.println("Valor do troco: R$"+troco);
-//OBS: Utilizei float, doble e não consegui trazer o valor exato/arredondado do troco (R$2,65).
+    public static void calcularTroco(double custo, double pagamento) {
+        BigDecimal valorCusto = new BigDecimal(Double.toString(custo));
+        BigDecimal valorPagamento = new BigDecimal(Double.toString(pagamento));
+        BigDecimal troco = valorPagamento.subtract(valorCusto);
+        System.out.printf("Valor do troco: R$%.2f%n", troco);
 
-        float[] notasEMoedas = { 100, 50, 20, 10, 5, 2, 1, 0.50f, 0.25f, 0.10f, 0.05f};
+        BigDecimal[] notasEMoedas = {  BigDecimal.valueOf(100), BigDecimal.valueOf(50), BigDecimal.valueOf(20),
+                BigDecimal.valueOf(10), BigDecimal.valueOf(5), BigDecimal.valueOf(2), BigDecimal.valueOf(1),
+                BigDecimal.valueOf(0.5), BigDecimal.valueOf(0.25), BigDecimal.valueOf(0.1), BigDecimal.valueOf(0.05),
+                BigDecimal.valueOf(0.01) };
 
         String[] nomeNotaMoeda = { "nota de 100", "nota de 50", "nota de 20", "nota de 10", "nota de 5",
                 "nota de 2", "nota de 1", "moeda de 50", "moeda de 25", "moeda de 10", "moeda de 05"};
@@ -21,12 +24,13 @@ public class ValorTroco {
         System.out.println("\nMenor troco:");
 
         for (int i = 0; i < notasEMoedas.length; i++) {
-            int quantidade = (int) (troco / notasEMoedas[i]);
+
+            BigDecimal[] divisao = troco.divideAndRemainder(notasEMoedas[i]);
+            int quantidade = divisao[0].intValue();
             if (quantidade > 0) {
                 System.out.println(quantidade + " - " + nomeNotaMoeda[i]);
-                troco = troco % notasEMoedas[i];
+                troco = divisao[1];
             }
         }
     }
-
 }
